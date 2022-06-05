c7a125d26e2035fc3661b8757df9529b09605f62
Juwana Zerman <juwanazerman@gmail.com>
2022-06-04 21:44:06 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -23,10 +23,9 @@ kubectl get service
 # Windows Machine: using the full pod name port forward it to port 4000 (port used in the yaml files)
 kubectl port-forward minikube-k8-challenge-5d66d945d8-zx8zw 4000:4000
 
-echo "Open new terminal window and run the following commands:"
+# "Open new terminal window and run the following commands:"
 
-# curl to check endpoint (http://localhost:4000/)
 curl http://localhost:4000/
 open http://localhost:4000/ # will open in the browser
-# OR In a new terminal window:
+
 
