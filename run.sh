df0b314d4c62139905d99dbdec6da2ed73f7f67c
Juwana Zerman <juwanazerman@gmail.com>
2022-06-04 21:41:30 -0400

    Update run.sh info

--- run.sh
+++ run.sh
@@ -15,19 +15,18 @@ docker build -t k8-challenge:latest .
 # deploy yaml files
 kubectl apply -f kubedeploy/
 
-# check cluster health with Kubernetes Dashboard
-minikube dashboard
-
-# check pods
-kubectl get pods
-
 # check services
 kubectl get service
 
-# get minikube ip
-minikube ip
-minikube service list
+# check pods
 
-# curl to check endpoint
+# Windows Machine: using the full pod name port forward it to port 4000 (port used in the yaml files)
+kubectl port-forward minikube-k8-challenge-5d66d945d8-zx8zw 4000:4000
 
+echo "Open new terminal window and run the following commands:"
+
+# curl to check endpoint (http://localhost:4000/)
+curl http://localhost:4000/
+open http://localhost:4000/ # will open in the browser
+# OR In a new terminal window:
 
